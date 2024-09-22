fc117caadc7953bb81df8f499ac558daa64165fd
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-09-22 20:06:19 +0300

    added HttpClient bean

---
+++ src/main/java/mate/academy/rickandmorty/config/HttpClientConfig.java
@@ -0,0 +1,13 @@
+package mate.academy.rickandmorty.config;
+
+import java.net.http.HttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpClientConfig {
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newHttpClient();
+    }
+}
